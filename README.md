8c8e4d88c3a27ebce513ea8bb0f1870ab17d8aa0
lightyears <lightyears1998@hotmail.com>
2019-10-01 17:57:30 +0800

    完成DescribeDomainRecords

--- README.md
+++ README.md
@@ -8,9 +8,16 @@
 # $Env:PIPENV_VENV_IN_PROJECT=1
 pipenv install
 cp config.example.yml config.yml
+# 在config.yml填入必要信息
+pipenv run python .
 ```
+
+## 建议
+
+1. 推荐使用[阿里云RAM](https://ram.console.aliyun.com/overview)子用户创建AccessKey。
 
 ## 参考
 
 - [阿里云云解析DNS文档](https://help.aliyun.com/document_detail/29740.html?spm=a2c4g.11186623.6.610.7a71120a6IvKWU)
 - [ipify.org](https://www.ipify.org/)
+- [DescribeDomainRecords接口文档](https://help.aliyun.com/document_detail/29776.html?spm=a2c4g.11186623.6.639.5ad9124fnH3B71)
--- __main__.py
+++ __main__.py
@@ -1,24 +1,53 @@
+import json
 import requests
 import yaml
 
 
 def main():
-    keyID, keySecret = load_config()
+    keyID, keySecret, domainName, recordName = load_config()
     ip = get_ipv4_address()
+    update_domain_record(keyID, keySecret, domainName, recordName)
 
 
 def load_config():
     with open('config.yml') as configFile:
         config = yaml.load(configFile, Loader=yaml.BaseLoader)
-        keyID, KeySecret = config["access-key"]['id'], config["access-key"]['secret']
-    return keyID, KeySecret
+        keyID, KeySecret = config["access-key"]["id"], config["access-key"]["secret"]
+        domainName, recordName = config["domain-name"], config["record-name"]
+    return keyID, KeySecret, domainName, recordName
 
 
-def get_ipv4_address() -> str:
+def get_ipv4_address():
     ip = requests.get("https://api.ipify.org").text
-    assert len(ip.split(".")) == 4
+    assert len(ip.split(".")) == 4, "无法查询IP地址"
     return ip
 
+
+def update_domain_record(keyID, keySecret, domainName, recordName):
+    record = get_domain_record(keyID, keySecret, domainName, recordName)
+    recordID = record["RecordId"]
+
+
+def get_domain_record(keyID, keySecret, domainName, recordName):
+    from aliyunsdkcore.client import AcsClient
+    from aliyunsdkalidns.request.v20150109.DescribeDomainRecordsRequest import DescribeDomainRecordsRequest
+
+    client = AcsClient(keyID, keySecret, 'cn-hangzhou')
+
+    request = DescribeDomainRecordsRequest()
+    request.set_accept_format('json')
+
+    request.set_DomainName(domainName)
+    request.set_RRKeyWord(recordName)
+
+    response = client.do_action_with_exception(request)
+    
+    records = json.loads(str(response, encoding='utf-8'))["DomainRecords"]["Record"]
+    records = filter(lambda record: record["RR"]==recordName and record["Type"]=="A", records)
+    records = list(records)
+    assert len(records) == 1, "找不到域名%s的A记录%s" % (domainName, recordName)
+
+    return records[0]
 
 if __name__ == "__main__":
     main()
